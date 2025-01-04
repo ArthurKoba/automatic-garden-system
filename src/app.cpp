8f17e29201b5dd8bedc587758d746982a192a725
ArthurKoba <caprimores@gmail.com>
2025-01-04 23:18:00 +0300

    improve update time algorithm

--- src/app.cpp
+++ src/app.cpp
@@ -6,7 +6,11 @@ void AutomaticGarden::_on_connect_wifi() {
     if (_ntp) return;
     _ntp = &NTP;
     _ntp->setHost(NTP_SERVER_DOMAIN);
-    _ntp->begin();
+    if (not _ntp->begin()) {
+        Serial.print(F("Failed start NTP."));
+        _ntp = nullptr;
+        return;
+    }
     _update_time_task(true);
 }
 
@@ -47,20 +51,43 @@ void AutomaticGarden::_wifi_task(bool forcibly) {
 
 void AutomaticGarden::_update_time_task(bool forcibly) {
     static uint32_t last_exec = 0;
-    if (need_skip_task_iteration(last_exec, 1000, forcibly)) return;
+    static uint32_t delay_iter_ms = 100;
+    if (need_skip_task_iteration(last_exec, delay_iter_ms, forcibly)) return;
 
     if (not _rtc.isrunning() and not _rtc.begin()) {
-        Serial.println(F("Failed running"));
+        Serial.println(F("Failed running RTC"));
+        delay_iter_ms = 100000;
         return;
     }
-    if (_last_wifi_status not_eq WL_CONNECTED or not _ntp or not _ntp->tick()) return;
+
+    if (_last_wifi_status not_eq WL_CONNECTED) return;
+
+    if (not _ntp) {
+        Serial.println(F("Failed running NTP Client"));
+        delay_iter_ms = 1000 * 60 * 60;
+        return;
+    }
+
+    if (_ntp->hasError()) {
+        Serial.print("NTP Error: ");
+        Serial.println(_ntp->readError());
+        if (_ntp->getError() not_eq GyverNTPClient::Error::Timeout) delay_iter_ms = 1000 * 60 * 60;
+    }
+
+    if (not _ntp->updateNow()) return;
 
     auto date_time_ntp = DateTime(_ntp->getUnix() + 3600 * TIMEZONE_OFFSET);
     auto date_time_rtc = _rtc.now();
 
     auto delta = float(date_time_ntp.unixtime()) - float(date_time_rtc.unixtime());
 
-    if (abs(delta) < 2) return;
+    if (delay_iter_ms == 100) {
+        show_time(date_time_ntp, F("The first time received from NTP: "));
+    }
+
+    delay_iter_ms = 1000 * 60;
+
+    if (date_time_ntp.year() > 2000 and abs(delta) < 2) return;
     show_time(date_time_ntp, F("Got time from NTP: "));
     show_time(date_time_rtc, F("Time from RTC: "));
 
@@ -74,8 +101,8 @@ bool AutomaticGarden::need_skip_task_iteration(
         uint32_t &last_iteration_time,
         uint32_t time_between_iterations_ms,
         bool forcibly
-    ) {
-    auto current_time= millis();
+) {
+    auto current_time = millis();
     if (current_time - last_iteration_time < time_between_iterations_ms and not forcibly) return true;
     last_iteration_time = current_time;
     return false;
@@ -118,13 +145,3 @@ void AutomaticGarden::loop() {
 //    auto value = analogRead(SOIL_MOISTURE_ANALOG_PIN);
 //    Serial.println(value);
 }
-
-
-
-
-
-
-
-
-
-
